2cb8e892b515b4203be9c4d874bd4d7c59cb23be
Michael D. M. Dryden <mdryden@chem.utoronto.ca>
2019-11-22 14:13:12 -0500

    adwaita-icon-theme: update to 3.34.3

--- recipes/adwaita-icon-theme/build.sh
+++ recipes/adwaita-icon-theme/build.sh
@@ -5,11 +5,11 @@
 [ "$NJOBS" = '<UNDEFINED>' -o -z "$NJOBS" ] && NJOBS=1
 set -e
 
-# For now, we have to manually update the gdk-pixbuf loader list:
-eval $(grep -v : $PREFIX/lib/pkgconfig/gdk-pixbuf-2.0.pc)
-$PREFIX/bin/gdk-pixbuf-query-loaders >$gdk_pixbuf_cache_file
+# # For now, we have to manually update the gdk-pixbuf loader list:
+# eval $(grep -v : $PREFIX/lib/pkgconfig/gdk-pixbuf-2.0.pc)
+# $PREFIX/bin/gdk-pixbuf-query-loaders >$gdk_pixbuf_cache_file
 
-configure_args=(--prefix=$PREFIX)
+configure_args=(--prefix=$PREFIX --disable-dependency-tracking)
 
 ./configure "${configure_args[@]}" || { cat config.log ; exit 1 ; }
 make -j$NJOBS
